dd1a6353ec42afc13bef78bdee79ce25458ad959
hariomop12 <hariomop12>
2024-11-30 13:21:04 +0530

    Operation ✌️

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -7,7 +7,6 @@ let valueInNumber = Number(score)
 //console.log(typeof valueInNumber); // number
 //console.log(valueInNumber); // NaN
 
-
 // "33" => 33
 // "33abc" => NaN
 //  true => 1; false => 0
