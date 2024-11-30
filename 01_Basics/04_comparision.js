15d11dbccb5bd9cf8cd8af37bdfd9fa7c4f05e2d
hariomop12 <hariomop12>
2024-11-30 13:24:56 +0530

    Comparision

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,16 @@
+console.log(1 > 2); // false
+console.log(1 < 2); // true
+console.log(1 >= 2); // false
+console.log(1 <= 2); // true
+console.log(1 == 2); // false
+console.log(1 != 2); // true
+console.log(1 === 2); // false
+console.log(1 !== 2); // true
+console.log(1 === "1"); // false    
+console.log(1 == "1"); // true
+console.log(1 != "1"); // false
+console.log(1 !== "1"); // true
+console.log(1 == true); // true
+console.log(1 === true); // false
+console.log(1 != true); // false
+console.log(1 !== true); // true
