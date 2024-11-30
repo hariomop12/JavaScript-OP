c2479a6e177ee218c703487c4c7d5ec7c98217ed
hariomop12 <hariomop12>
2024-11-30 22:24:26 +0530

    JSON BABY 😍

---
+++ 02_Basics/05_JSON.js
@@ -0,0 +1,24 @@
+const course = {
+  course: "JavaScript",
+  lessons: 40,
+  price: 3000,
+};
+
+const { lessons, price } = course;
+// destructuring the object is a way to extract the values from the object and store them in variables
+// eg - const {lessons, price} = course;
+// this will extract the lessons and price from the course object and store them in lessons and price variables respectively
+console.log(lessons, price); // 40 3000
+
+// object with out name is JSON
+// {
+
+// }
+
+const OP = {
+  course: "JavaScript",
+  lessons: "40",
+  price: "3000",
+}
+
+[({}, {}, {})];
