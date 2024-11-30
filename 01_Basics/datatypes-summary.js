8bbb4030c1e87042811323fba4a7a31c6f052821
hariomop12 <hariomop12>
2024-11-30 13:45:20 +0530

    Data types Summary 🤷‍♂️

---
+++ 01_Basics/datatypes-summary.js
@@ -0,0 +1,15 @@
+// primitive data types(7) : call by value(not reference of memory instead value its copy of value)
+// 1. string    : "User" // return string (using typeof operator *)
+// 2. number    : 33    // return number
+// 3. bigint    : 123456789012345678901234567890123456789054444444444444444789465 // return bigint
+// 4. boolean   : true / false // return boolean
+// 5. null      : null // return object
+// 6. undefined : undefined // return undefined
+// 7. symbol    : Symbol("id") // for making unique value // return symbol
+
+// non-primitive data types(3) : call by reference (reference of memory) // return object
+// 1. object    : {} // return object
+// 2. array     : [] // return object
+// 3. function  : function() {} // return function
+
+// javascript is dynamically typed language (we don't have to specify the data type of a variable when we declare it)
