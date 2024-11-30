a5167b91a0f45baca3c6bc69f104f3d74199ab52
hariomop12 <hariomop12>
2024-11-30 13:09:01 +0530

    Conversions 🤞

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "Hariom"
+
+//console.log(typeof score); // string
+//console.log(typeof(score)); // string
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber); // number
+//console.log(valueInNumber); // NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hariom"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Hariom" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
