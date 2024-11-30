81fbbcace34ef3ad2a83e4599deab5de075920fe
hariomop12 <hariomop12>
2024-11-30 14:34:22 +0530

    Numbs and Maths 🙌

---
+++ 01_Basics/06.nums_and_maths.js
@@ -0,0 +1,37 @@
+// const score = 100;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+// const anotherScore = 200.545;
+// console.log(anotherScore.toPrecision(5));
+
+
+
+// const hundred = 100000000
+// console.log(hundred.toExponential(2));
+// console.log(hundred.toLocaleString('en-IN'));
+
+
+
+// +++++++++++Maths+++++++++++++++
+
+// console.log(Math.PI);
+// console.log(Math.E);
+// console.log(Math.round(2.4));
+// console.log(Math.ceil(2.4));
+// console.log(Math.floor(2.4));
+// console.log(Math.trunc(2.4));
+// console.log(Math.sqrt(64));
+// console.log(Math.abs(-3));
+
+
+console.log(Math.random());
+console.log((Math.random() * 10) + 1);
+console.log(Math.floor(Math.random() * 10) + 1);
